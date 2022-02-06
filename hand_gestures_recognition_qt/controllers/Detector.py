ce67354bf245d58cd120951887fa2fc0ac34ea4e
Mikołaj Sawicki <msawicki9999@gmail.com>
2022-02-06 16:54:16 +0100

    Draft

--- hand_gestures_recognition_qt/controllers/Detector.py
+++ hand_gestures_recognition_qt/controllers/Detector.py
@@ -1,6 +1,19 @@
 from PyQt5.QtCore import QObject
 import numpy as np
 from hand_gestures_recognition.mediapipe.MPGestureRecognition import MPGestureRecognition
+from multiprocessing import Process, Queue
+
+
+def recognition_process(queue: Queue):
+    recognition = MPGestureRecognition()
+    alive = True
+
+    while alive:
+        if not queue.empty():
+            msg = queue.get()
+
+
+
 
 
 class Detector(QObject):
@@ -8,6 +21,8 @@ class Detector(QObject):
         super().__init__()
         self._recognition = MPGestureRecognition()
         self._available = True
+        self._process = Process(target=self._recognition_loop)
+        self._process.start()
 
     def recogniseImage(self, img: np.ndarray):
         if self._available:
@@ -36,3 +51,60 @@ class Detector(QObject):
 
     def flush(self):
         pass
+
+
+from PyQt5 import QtCore
+
+# Runner lives on the runner thread
+
+
+class Runner(QtCore.QObject):
+    """
+    Runs a job in a separate process and forwards messages from the job to the
+    main thread through a pyqtSignal.
+
+    """
+
+    msg_from_job = QtCore.pyqtSignal(object)
+
+    def __init__(self, start_signal):
+        """
+        :param start_signal: the pyqtSignal that starts the job
+
+        """
+        super(Runner, self).__init__()
+        self.job_input = None
+        start_signal.connect(self._run)
+
+    def _run(self):
+        queue = Queue()
+        p = Process(target=job_function, args=(queue, self.job_input))
+        p.start()
+        while True:
+            msg = queue.get()
+            self.msg_from_job.emit(msg)
+            if msg == 'done':
+                break
+
+
+# Things below live on the main thread
+
+def run_job(input):
+    """ Call this to start a new job """
+    runner.job_input = input
+    runner_thread.start()
+
+
+def handle_msg(msg):
+    print(msg)
+    if msg == 'done':
+        runner_thread.quit()
+        runner_thread.wait()
+
+
+# Setup the OQ listener thread and move the OQ runner object to it
+runner_thread = QtCore.QThread()
+runner = Runner(start_signal=runner_thread.started)
+runner.msg_from_job.connect(handle_msg)
+runner.moveToThread(runner_thread)
+
